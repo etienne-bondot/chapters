c68f92690890197b74a170e24389e53e582a6c72
Etienne BONDOT <etiennebondot@gmail.com>
2016-01-27 15:52:31 +0000

    Explicit and add some custom options

--- app/components/typedjs/typedjs-directive.js
+++ app/components/typedjs/typedjs-directive.js
@@ -12,10 +12,36 @@
 		function link(scope, elem, attrs) {
 			var options = {
 				strings: scope.strings,
-				typeSpeed: 2,
-				contentType: "html",
-				showCursor: true,
-				cursorChar: "="
+			   // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			   stringsElement: null,
+			   // typing speed
+			   typeSpeed: 2,
+			   // time before typing starts
+			   startDelay: 0,
+			   // backspacing speed
+			   backSpeed: 0,
+			   // time before backspacing
+			   backDelay: 500,
+			   // loop
+			   loop: false,
+			   // false = infinite
+			   loopCount: false,
+			   // show cursor
+			   showCursor: true,
+			   // character for cursor
+			   cursorChar: "|",
+			   // attribute to type (null == text)
+			   attr: null,
+			   // either html or text
+			   contentType: 'html',
+			   // call when done callback function
+			   callback: function() {},
+			   // starting callback function before each string
+			   preStringTyped: function() {},
+			   //callback for every typed string
+			   onStringTyped: function() {},
+			   // callback for reset
+			   resetCallback: function() {}
 			};
 
 			$(function() {
